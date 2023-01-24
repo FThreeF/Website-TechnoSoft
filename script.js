2ee647eb88f0416bd094b98540bb0a4c59589e49
FThreeF <FThreeF.535@gmail.com>
2023-01-24 19:50:16 +0300

    0.2

--- script.js
+++ script.js
@@ -35,21 +35,3 @@ function scrollTo(element) {
 
 
 
-function onEntry(entry) {
-    entry.forEach(change => {
-        change.target.classList.add(element-show);
-    })
-}
-
-
-let options = {
-    threshold: [0.5]
-}
-
-let observer = new IntersectionObserver(onEntry, options)
-
-let elements = document.querySelectorAll('.element-animation')
-
-for (let elm of elements) {
-    observer.observe(elm);
-}
